eedb829b9a8feb05d5507d8264374e37fd33a69f
Ludger Winkels <lwin@d-velop.de>
2022-05-12 13:10:31 +0200

    MVC based server added

---
+++ MVCServer/Controllers/AddressDataContoller.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ValueObjects;
+
+namespace MVCServer.Controllers
+{
+    [ApiController]
+    [Route("adressdata")]
+    public class AddressDataContoller : ControllerBase
+    {
+        private AddressData _addressData = new AddressData("Max", "Musermann", "Musterweg 1", "Musterort", "Germany", "12345");
+
+        private readonly ILogger<AddressDataContoller> _logger;
+
+        public AddressDataContoller(ILogger<AddressDataContoller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public AddressData Get()
+        {
+            return _addressData;
+        }
+    }
+}
